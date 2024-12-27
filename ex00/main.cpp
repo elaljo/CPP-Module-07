af03c38bec83e1c448dae3433d959c0828589d50
Mohamed Elalj <moelalj@c1r4p8.1337.ma>
2024-12-27 17:47:58 +0100

    done

---
+++ ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moelalj <moelalj@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/27 13:50:09 by moelalj           #+#    #+#             */
+/*   Updated: 2024/12/27 17:47:42 by moelalj          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main(void)
+{
+    int a = 2;
+    int b = 3;
+    
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    return 0;
+}
---
+++ ex00/whatever.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moelalj <moelalj@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/27 13:50:23 by moelalj           #+#    #+#             */
+/*   Updated: 2024/12/27 17:46:41 by moelalj          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+    void swap(T &a, T &b){
+            T tmp = a;
+            a = b;
+            b = tmp;
+        }
+template <typename T>
+    T min(T a, T b){
+        if (a == b)
+            return b;
+        else if (a > b)
+            return b;
+        else
+            return a;
+    }
+template <typename T>
+    T max(T a, T b){
+        if (a == b)
+            return b;
+        else if (a > b)
+            return a;
+        else
+            return b;
+    }
+    
+#endif
